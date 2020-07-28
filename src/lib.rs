9abe167962fc975de10f4233eecd76f4ca81f60c
Stefano Chiodino <not@going.2.happen>
2020-07-28 22:48:31 +0100

    reset and try again

--- src/lib.rs
+++ src/lib.rs
@@ -1,66 +1,33 @@
 mod simple_db {
-    use regex::Regex;
-    use std::fs;
-    use std::fs::{DirEntry, File};
-    use std::path::{Path, PathBuf};
-    use uuid::Uuid;
-
-    pub struct Client {
-        name: String,
-    }
+    use std::collections::HashMap;
+    use std::fs::File;
 
     pub enum Errors {
         NotFound,
     }
 
-    impl Client {
-        #[allow(dead_code)]
-        pub fn new(name: String) -> Client {
-            println!("Initialised client with name '{}'", name);
-            Client { name }
-        }
+    pub(crate) struct Db {
+        name: String,
+        data: HashMap<String, u32>,
+        // indexes: HashMap<T, dyn Fn(&str) -> T>,
+    }
 
-        #[allow(dead_code)]
-        pub fn post<T: serde::ser::Serialize>(&self, obj: T) -> Result<String, Errors> {
-            let folder_path = self.get_folder::<T>();
-            let id = Uuid::new_v4().to_string();
-            let file_path = folder_path.join(Path::new(&id.to_string()));
-            let mut file = File::create(file_path).unwrap();
-
-            bincode::serialize_into(&mut file, &obj).unwrap();
-            Ok(id)
-        }
-
-        #[allow(dead_code)]
-        pub fn get<T: serde::de::DeserializeOwned>(&self, id: &String) -> Result<T, Errors> {
-            let folder_path = self.get_folder::<T>();
-            let file_path = folder_path.join(Path::new(id));
-            let read_results = &fs::read(&file_path);
-            match read_results {
-                Ok(read_bytes) => {
-                    let result = bincode::deserialize(&read_bytes);
-                    let obj = result.unwrap();
-                    Ok(obj)
-                }
-                Err(_) => Err(Errors::NotFound),
+    impl Db {
+        pub fn new(name: String) -> Self {
+            Self {
+                name,
+                data: HashMap::new(),
             }
         }
 
         #[allow(dead_code)]
         pub fn nuke(&self) -> Result<(), Errors> {
-            let seeded_folder = Path::new(self.name.as_str());
-            fs::remove_dir_all(seeded_folder).unwrap();
-            Ok(())
+            Err(Errors::NotFound)
         }
 
         #[allow(dead_code)]
         pub fn delete<T: serde::de::DeserializeOwned>(&self, id: &String) -> Result<(), Errors> {
-            let folder_path = self.get_folder::<T>();
-            let file_path = folder_path.join(Path::new(id));
-            match fs::remove_file(file_path) {
-                Ok(_) => Ok(()),
-                Err(_) => Err(Errors::NotFound),
-            }
+            Err(Errors::NotFound)
         }
 
         #[allow(dead_code)]
@@ -69,17 +36,7 @@ mod simple_db {
             predicate: fn(&T) -> bool,
             limit: usize,
         ) -> Result<Option<Vec<T>>, Errors> {
-            let folder_path = self.get_folder::<T>();
-            let directory_read = fs::read_dir(folder_path).ok().unwrap();
-            let matches: Vec<T> = directory_read
-                .filter_map(|x| dir_entry_matches_predicate::<T>(x.unwrap(), predicate))
-                .take(limit)
-                .collect();
-            return if matches.is_empty() {
-                Ok(None)
-            } else {
-                Ok(Some(matches))
-            };
+            Err(Errors::NotFound)
         }
 
         #[allow(dead_code)]
@@ -87,193 +44,153 @@ mod simple_db {
             &self,
             predicate: fn(&T) -> bool,
         ) -> Result<Option<T>, Errors> {
-            match self.find(predicate, 1) {
-                Ok(items_option) => match items_option {
-                    Some(items) => Ok(Some(items.into_iter().nth(0).unwrap())),
-                    None => Ok(None),
-                },
-                Err(_) => Err(Errors::NotFound),
+            Err(Errors::NotFound)
+        }
+
+        #[allow(dead_code)]
+        pub fn get<T: serde::de::DeserializeOwned>(&self, id: &String) -> Result<T, Errors> {
+            Err(Errors::NotFound)
+        }
+
+        #[allow(dead_code)]
+        pub fn post<T: serde::ser::Serialize>(&self, obj: T) -> Result<String, Errors> {
+            Err(Errors::NotFound)
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use crate::simple_db::*;
+        use serde::Deserialize;
+        use serde::Serialize;
+        use std::collections::HashMap;
+        use std::panic;
+        use uuid::Uuid;
+
+        fn seeded_db() -> Db {
+            Db::new(Uuid::new_v4().to_string())
+        }
+
+        // use crate::simple_db::Crud;
+        #[test]
+        fn safe_filename() {
+            let mut pairs: HashMap<&str, &str> = HashMap::new();
+            pairs.insert("test", "test");
+            pairs.insert("a1", "a1");
+            // pairs.insert("!@£^@!$£", "_");
+            pairs.insert("test!@£$123", "test123");
+            pairs.insert(std::any::type_name::<str>(), "str");
+
+            for (input, expected_output) in pairs {
+                // let actual_output = to_safe_filename(input);
+                // assert_eq!(actual_output, expected_output);
             }
         }
 
-        pub fn get_seeded_folder(&self) -> PathBuf {
-            let seed_folder = Path::new(self.name.as_str());
-            let base_of_data_path = seed_folder.join("base_of_data");
-            if base_of_data_path.exists() == false {
-                fs::create_dir_all(&base_of_data_path).unwrap();
+        #[test]
+        fn create_db() {
+            let _db = seeded_db();
+        }
+
+        #[test]
+        fn post() {
+            let db = seeded_db();
+            db.post("hello").ok().unwrap();
+            db.nuke().ok().unwrap();
+        }
+
+        #[test]
+        fn get() {
+            let db = seeded_db();
+            let id = db.post::<String>("hello".to_string()).ok().unwrap();
+            let actual = db.get::<String>(&id).ok().unwrap();
+            assert_eq!(actual, "hello");
+            db.nuke().ok().unwrap();
+        }
+
+        #[test]
+        fn multiple_get() {
+            let db = seeded_db();
+            let id1 = db.post::<String>("hello1".to_string()).ok().unwrap();
+            let actual1 = db.get::<String>(&id1).ok().unwrap();
+            assert_eq!(actual1, "hello1");
+            let id2 = db.post::<String>("hello2".to_string()).ok().unwrap();
+            let actual2 = db.get::<String>(&id2).ok().unwrap();
+            assert_eq!(actual2, "hello2");
+            db.nuke().ok().unwrap();
+        }
+
+        #[test]
+        fn nuke() {
+            let db = seeded_db();
+            let id = db.post::<String>("hello".to_string()).ok().unwrap();
+            let actual = db.nuke();
+            assert!(actual.is_ok());
+            assert!(db.get::<String>(&id).is_err());
+            db.nuke().ok().unwrap();
+        }
+
+        #[test]
+        fn delete() {
+            let db = seeded_db();
+            let id = db.post::<String>("hello".to_string()).ok().unwrap();
+            assert!(db.get::<String>(&id).is_ok());
+            db.delete::<String>(&id).ok().unwrap();
+            assert!(db.get::<String>(&id).is_err());
+            db.nuke().ok().unwrap();
+        }
+
+        #[test]
+        fn delete_non_existing_id() {
+            let db = seeded_db();
+            let result = db.delete::<String>(&"made_up".to_string());
+            assert!(result.is_err());
+            db.nuke().ok().unwrap();
+        }
+
+        #[test]
+        fn complex_object_workflow() {
+            #[derive(Serialize, Deserialize, PartialEq, Debug)]
+            struct Complex {
+                name: String,
+                x: i32,
             }
-            base_of_data_path
+            let complex = Complex {
+                name: "Stefano".to_string(),
+                x: 34,
+            };
+            let db = seeded_db();
+            let id = db.post(&complex).ok().unwrap();
+            let retrieved_complex = db.get::<Complex>(&id).ok().unwrap();
+            assert_eq!(retrieved_complex, complex);
+            db.delete::<Complex>(&id).ok().unwrap();
+            assert!(db.get::<Complex>(&id).is_err());
+            db.nuke().ok().unwrap();
         }
-        pub fn get_folder<T>(&self) -> PathBuf {
-            let base_of_data_path = self.get_seeded_folder();
-            let type_name = std::any::type_name::<T>();
-            let safe_type_name = to_safe_filename(type_name);
-            let folder_path = base_of_data_path.join(safe_type_name);
-            if folder_path.exists() == false {
-                fs::create_dir_all(&folder_path).unwrap();
-            }
-            println!("Got folder with path '{}'", folder_path.display());
-            folder_path
+
+        #[test]
+        fn find() {
+            let db = seeded_db();
+            db.post::<String>("hello".to_string()).ok().unwrap();
+            let actual = db
+                .find_one::<String>(|x: &String| x.starts_with("hell"))
+                .ok()
+                .unwrap()
+                .unwrap();
+            assert_eq!(actual, "hello");
+            db.nuke().ok().unwrap();
         }
-    }
 
-    fn dir_entry_matches_predicate<T: serde::de::DeserializeOwned>(
-        dir_entry: DirEntry,
-        predicate: fn(&T) -> bool,
-    ) -> Option<T> {
-        let read_results = &fs::read(&dir_entry.path());
-        match read_results {
-            Ok(read_bytes) => {
-                let result = bincode::deserialize(&read_bytes);
-                let obj = result.unwrap();
-                if predicate(&obj) {
-                    Some(obj)
-                } else {
-                    None
-                }
-            }
-            Err(_) => None,
+        #[test]
+        fn not_found() {
+            let db = seeded_db();
+            db.post::<String>("hello".to_string()).ok().unwrap();
+            let actual = db
+                .find_one::<String>(|x: &String| x.starts_with("hellllooo"))
+                .ok()
+                .unwrap();
+            assert!(actual.is_none());
+            db.nuke().ok().unwrap();
         }
-    }
-
-    pub fn to_safe_filename(input: &str) -> String {
-        let re = Regex::new(r"[^\w\d]").unwrap();
-        re.replace_all(input, "").to_string()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::simple_db::*;
-    use serde::Deserialize;
-    use serde::Serialize;
-    use std::collections::HashMap;
-    use std::panic;
-    use uuid::Uuid;
-
-    fn seeded_client() -> Client {
-        Client::new(Uuid::new_v4().to_string())
-    }
-
-    // use crate::simple_db::Crud;
-    #[test]
-    fn safe_filename() {
-        let mut pairs: HashMap<&str, &str> = HashMap::new();
-        pairs.insert("test", "test");
-        pairs.insert("a1", "a1");
-        // pairs.insert("!@£^@!$£", "_");
-        pairs.insert("test!@£$123", "test123");
-        pairs.insert(std::any::type_name::<str>(), "str");
-
-        for (input, expected_output) in pairs {
-            let actual_output = to_safe_filename(input);
-            assert_eq!(actual_output, expected_output);
-        }
-    }
-
-    #[test]
-    fn create_client() {
-        let _client = seeded_client();
-    }
-
-    #[test]
-    fn post() {
-        let client = seeded_client();
-        client.post("hello").ok().unwrap();
-        client.nuke().ok().unwrap();
-    }
-
-    #[test]
-    fn get() {
-        let client = seeded_client();
-        let id = client.post::<String>("hello".to_string()).ok().unwrap();
-        let actual = client.get::<String>(&id).ok().unwrap();
-        assert_eq!(actual, "hello");
-        client.nuke().ok().unwrap();
-    }
-
-    #[test]
-    fn multiple_get() {
-        let client = seeded_client();
-        let id1 = client.post::<String>("hello1".to_string()).ok().unwrap();
-        let actual1 = client.get::<String>(&id1).ok().unwrap();
-        assert_eq!(actual1, "hello1");
-        let id2 = client.post::<String>("hello2".to_string()).ok().unwrap();
-        let actual2 = client.get::<String>(&id2).ok().unwrap();
-        assert_eq!(actual2, "hello2");
-        client.nuke().ok().unwrap();
-    }
-
-    #[test]
-    fn nuke() {
-        let client = seeded_client();
-        let id = client.post::<String>("hello".to_string()).ok().unwrap();
-        let actual = client.nuke();
-        assert!(actual.is_ok());
-        assert!(client.get::<String>(&id).is_err());
-        client.nuke().ok().unwrap();
-    }
-
-    #[test]
-    fn delete() {
-        let client = seeded_client();
-        let id = client.post::<String>("hello".to_string()).ok().unwrap();
-        assert!(client.get::<String>(&id).is_ok());
-        client.delete::<String>(&id).ok().unwrap();
-        assert!(client.get::<String>(&id).is_err());
-        client.nuke().ok().unwrap();
-    }
-
-    #[test]
-    fn delete_non_existing_id() {
-        let client = seeded_client();
-        let result = client.delete::<String>(&"made_up".to_string());
-        assert!(result.is_err());
-        client.nuke().ok().unwrap();
-    }
-
-    #[test]
-    fn complex_object_workflow() {
-        #[derive(Serialize, Deserialize, PartialEq, Debug)]
-        struct Complex {
-            name: String,
-            x: i32,
-        }
-        let complex = Complex {
-            name: "Stefano".to_string(),
-            x: 34,
-        };
-        let client = seeded_client();
-        let id = client.post(&complex).ok().unwrap();
-        let retrieved_complex = client.get::<Complex>(&id).ok().unwrap();
-        assert_eq!(retrieved_complex, complex);
-        client.delete::<Complex>(&id).ok().unwrap();
-        assert!(client.get::<Complex>(&id).is_err());
-        client.nuke().ok().unwrap();
-    }
-
-    #[test]
-    fn find() {
-        let client = seeded_client();
-        client.post::<String>("hello".to_string()).ok().unwrap();
-        let actual = client
-            .find_one::<String>(|x: &String| x.starts_with("hell"))
-            .ok()
-            .unwrap()
-            .unwrap();
-        assert_eq!(actual, "hello");
-        client.nuke().ok().unwrap();
-    }
-
-    #[test]
-    fn not_found() {
-        let client = seeded_client();
-        client.post::<String>("hello".to_string()).ok().unwrap();
-        let actual = client
-            .find_one::<String>(|x: &String| x.starts_with("hellllooo"))
-            .ok()
-            .unwrap();
-        assert!(actual.is_none());
-        client.nuke().ok().unwrap();
     }
 }
---
+++ src/lib2.rs
@@ -0,0 +1,279 @@
+mod simple_db {
+    use regex::Regex;
+    use std::fs;
+    use std::fs::{DirEntry, File};
+    use std::path::{Path, PathBuf};
+    use uuid::Uuid;
+
+    pub struct Client {
+        name: String,
+    }
+
+    pub enum Errors {
+        NotFound,
+    }
+
+    impl Client {
+        #[allow(dead_code)]
+        pub fn new(name: String) -> Client {
+            println!("Initialised client with name '{}'", name);
+            Client { name }
+        }
+
+        #[allow(dead_code)]
+        pub fn post<T: serde::ser::Serialize>(&self, obj: T) -> Result<String, Errors> {
+            let folder_path = self.get_folder::<T>();
+            let id = Uuid::new_v4().to_string();
+            let file_path = folder_path.join(Path::new(&id.to_string()));
+            let mut file = File::create(file_path).unwrap();
+
+            bincode::serialize_into(&mut file, &obj).unwrap();
+            Ok(id)
+        }
+
+        #[allow(dead_code)]
+        pub fn get<T: serde::de::DeserializeOwned>(&self, id: &String) -> Result<T, Errors> {
+            let folder_path = self.get_folder::<T>();
+            let file_path = folder_path.join(Path::new(id));
+            let read_results = &fs::read(&file_path);
+            match read_results {
+                Ok(read_bytes) => {
+                    let result = bincode::deserialize(&read_bytes);
+                    let obj = result.unwrap();
+                    Ok(obj)
+                }
+                Err(_) => Err(Errors::NotFound),
+            }
+        }
+
+        #[allow(dead_code)]
+        pub fn nuke(&self) -> Result<(), Errors> {
+            let seeded_folder = Path::new(self.name.as_str());
+            fs::remove_dir_all(seeded_folder).unwrap();
+            Ok(())
+        }
+
+        #[allow(dead_code)]
+        pub fn delete<T: serde::de::DeserializeOwned>(&self, id: &String) -> Result<(), Errors> {
+            let folder_path = self.get_folder::<T>();
+            let file_path = folder_path.join(Path::new(id));
+            match fs::remove_file(file_path) {
+                Ok(_) => Ok(()),
+                Err(_) => Err(Errors::NotFound),
+            }
+        }
+
+        #[allow(dead_code)]
+        pub fn find<T: serde::de::DeserializeOwned>(
+            &self,
+            predicate: fn(&T) -> bool,
+            limit: usize,
+        ) -> Result<Option<Vec<T>>, Errors> {
+            let folder_path = self.get_folder::<T>();
+            let directory_read = fs::read_dir(folder_path).ok().unwrap();
+            let matches: Vec<T> = directory_read
+                .filter_map(|x| dir_entry_matches_predicate::<T>(x.unwrap(), predicate))
+                .take(limit)
+                .collect();
+            return if matches.is_empty() {
+                Ok(None)
+            } else {
+                Ok(Some(matches))
+            };
+        }
+
+        #[allow(dead_code)]
+        pub fn find_one<T: serde::de::DeserializeOwned>(
+            &self,
+            predicate: fn(&T) -> bool,
+        ) -> Result<Option<T>, Errors> {
+            match self.find(predicate, 1) {
+                Ok(items_option) => match items_option {
+                    Some(items) => Ok(Some(items.into_iter().nth(0).unwrap())),
+                    None => Ok(None),
+                },
+                Err(_) => Err(Errors::NotFound),
+            }
+        }
+
+        pub fn get_seeded_folder(&self) -> PathBuf {
+            let seed_folder = Path::new(self.name.as_str());
+            let base_of_data_path = seed_folder.join("base_of_data");
+            if base_of_data_path.exists() == false {
+                fs::create_dir_all(&base_of_data_path).unwrap();
+            }
+            base_of_data_path
+        }
+        pub fn get_folder<T>(&self) -> PathBuf {
+            let base_of_data_path = self.get_seeded_folder();
+            let type_name = std::any::type_name::<T>();
+            let safe_type_name = to_safe_filename(type_name);
+            let folder_path = base_of_data_path.join(safe_type_name);
+            if folder_path.exists() == false {
+                fs::create_dir_all(&folder_path).unwrap();
+            }
+            println!("Got folder with path '{}'", folder_path.display());
+            folder_path
+        }
+    }
+
+    fn dir_entry_matches_predicate<T: serde::de::DeserializeOwned>(
+        dir_entry: DirEntry,
+        predicate: fn(&T) -> bool,
+    ) -> Option<T> {
+        let read_results = &fs::read(&dir_entry.path());
+        match read_results {
+            Ok(read_bytes) => {
+                let result = bincode::deserialize(&read_bytes);
+                let obj = result.unwrap();
+                if predicate(&obj) {
+                    Some(obj)
+                } else {
+                    None
+                }
+            }
+            Err(_) => None,
+        }
+    }
+
+    pub fn to_safe_filename(input: &str) -> String {
+        let re = Regex::new(r"[^\w\d]").unwrap();
+        re.replace_all(input, "").to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::simple_db::*;
+    use serde::Deserialize;
+    use serde::Serialize;
+    use std::collections::HashMap;
+    use std::panic;
+    use uuid::Uuid;
+
+    fn seeded_client() -> Client {
+        Client::new(Uuid::new_v4().to_string())
+    }
+
+    // use crate::simple_db::Crud;
+    #[test]
+    fn safe_filename() {
+        let mut pairs: HashMap<&str, &str> = HashMap::new();
+        pairs.insert("test", "test");
+        pairs.insert("a1", "a1");
+        // pairs.insert("!@£^@!$£", "_");
+        pairs.insert("test!@£$123", "test123");
+        pairs.insert(std::any::type_name::<str>(), "str");
+
+        for (input, expected_output) in pairs {
+            let actual_output = to_safe_filename(input);
+            assert_eq!(actual_output, expected_output);
+        }
+    }
+
+    #[test]
+    fn create_client() {
+        let _client = seeded_client();
+    }
+
+    #[test]
+    fn post() {
+        let client = seeded_client();
+        client.post("hello").ok().unwrap();
+        client.nuke().ok().unwrap();
+    }
+
+    #[test]
+    fn get() {
+        let client = seeded_client();
+        let id = client.post::<String>("hello".to_string()).ok().unwrap();
+        let actual = client.get::<String>(&id).ok().unwrap();
+        assert_eq!(actual, "hello");
+        client.nuke().ok().unwrap();
+    }
+
+    #[test]
+    fn multiple_get() {
+        let client = seeded_client();
+        let id1 = client.post::<String>("hello1".to_string()).ok().unwrap();
+        let actual1 = client.get::<String>(&id1).ok().unwrap();
+        assert_eq!(actual1, "hello1");
+        let id2 = client.post::<String>("hello2".to_string()).ok().unwrap();
+        let actual2 = client.get::<String>(&id2).ok().unwrap();
+        assert_eq!(actual2, "hello2");
+        client.nuke().ok().unwrap();
+    }
+
+    #[test]
+    fn nuke() {
+        let client = seeded_client();
+        let id = client.post::<String>("hello".to_string()).ok().unwrap();
+        let actual = client.nuke();
+        assert!(actual.is_ok());
+        assert!(client.get::<String>(&id).is_err());
+        client.nuke().ok().unwrap();
+    }
+
+    #[test]
+    fn delete() {
+        let client = seeded_client();
+        let id = client.post::<String>("hello".to_string()).ok().unwrap();
+        assert!(client.get::<String>(&id).is_ok());
+        client.delete::<String>(&id).ok().unwrap();
+        assert!(client.get::<String>(&id).is_err());
+        client.nuke().ok().unwrap();
+    }
+
+    #[test]
+    fn delete_non_existing_id() {
+        let client = seeded_client();
+        let result = client.delete::<String>(&"made_up".to_string());
+        assert!(result.is_err());
+        client.nuke().ok().unwrap();
+    }
+
+    #[test]
+    fn complex_object_workflow() {
+        #[derive(Serialize, Deserialize, PartialEq, Debug)]
+        struct Complex {
+            name: String,
+            x: i32,
+        }
+        let complex = Complex {
+            name: "Stefano".to_string(),
+            x: 34,
+        };
+        let client = seeded_client();
+        let id = client.post(&complex).ok().unwrap();
+        let retrieved_complex = client.get::<Complex>(&id).ok().unwrap();
+        assert_eq!(retrieved_complex, complex);
+        client.delete::<Complex>(&id).ok().unwrap();
+        assert!(client.get::<Complex>(&id).is_err());
+        client.nuke().ok().unwrap();
+    }
+
+    #[test]
+    fn find() {
+        let client = seeded_client();
+        client.post::<String>("hello".to_string()).ok().unwrap();
+        let actual = client
+            .find_one::<String>(|x: &String| x.starts_with("hell"))
+            .ok()
+            .unwrap()
+            .unwrap();
+        assert_eq!(actual, "hello");
+        client.nuke().ok().unwrap();
+    }
+
+    #[test]
+    fn not_found() {
+        let client = seeded_client();
+        client.post::<String>("hello".to_string()).ok().unwrap();
+        let actual = client
+            .find_one::<String>(|x: &String| x.starts_with("hellllooo"))
+            .ok()
+            .unwrap();
+        assert!(actual.is_none());
+        client.nuke().ok().unwrap();
+    }
+}
