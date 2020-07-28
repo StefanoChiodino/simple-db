84c029c8eb05dbfa463e98b016b7b9f2b895a32f
Stefano Chiodino <not@going.2.happen>
2020-07-28 23:15:08 +0100

    create and nuke

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 mod simple_db {
-    use std::collections::HashMap;
+    use std::collections::{BTreeMap, HashMap};
     use std::fs::File;
+    use std::io::{Seek, SeekFrom};
+    use std::path::Path;
+    use uuid::Uuid;
 
     pub enum Errors {
         NotFound,
@@ -8,16 +11,32 @@ mod simple_db {
 
     pub(crate) struct Db {
         name: String,
-        data: HashMap<String, u32>,
+        data_map: BTreeMap<String, (u32, u32)>,
+        file: File,
         // indexes: HashMap<T, dyn Fn(&str) -> T>,
     }
 
     impl Db {
         pub fn new(name: String) -> Self {
             Self {
-                name,
-                data: HashMap::new(),
+                name: name.to_string(),
+                data_map: BTreeMap::new(),
+                file: File::create(Path::new(&format!("{}.sdb", name.as_str()))).unwrap(),
             }
+        }
+
+        #[allow(dead_code)]
+        pub fn post<T: serde::ser::Serialize>(&mut self, obj: T) -> Result<String, Errors> {
+            let new_id = Uuid::new_v4().to_string();
+            let data_location = if self.data_map.is_empty() {
+                0
+            } else {
+                let (last_item_position, last_item_length) = self.data_map.iter().last().unwrap().1;
+                last_item_position + last_item_length + 1
+            };
+            self.file.seek(SeekFrom::Start(data_location as u64));
+            bincode::serialize_into(&mut self.file, &obj).unwrap();
+            Ok(new_id)
         }
 
         #[allow(dead_code)]
@@ -51,11 +70,6 @@ mod simple_db {
         pub fn get<T: serde::de::DeserializeOwned>(&self, id: &String) -> Result<T, Errors> {
             Err(Errors::NotFound)
         }
-
-        #[allow(dead_code)]
-        pub fn post<T: serde::ser::Serialize>(&self, obj: T) -> Result<String, Errors> {
-            Err(Errors::NotFound)
-        }
     }
 
     #[cfg(test)]
@@ -64,11 +78,16 @@ mod simple_db {
         use serde::Deserialize;
         use serde::Serialize;
         use std::collections::HashMap;
+        use std::fs;
         use std::panic;
         use uuid::Uuid;
 
         fn seeded_db() -> Db {
             Db::new(Uuid::new_v4().to_string())
+        }
+
+        fn nuke_db(db: Db) {
+            fs::remove_file(format!("{}.sdb", db.name));
         }
 
         // use crate::simple_db::Crud;
@@ -89,55 +108,56 @@ mod simple_db {
 
         #[test]
         fn create_db() {
-            let _db = seeded_db();
+            let db = seeded_db();
+            nuke_db(db);
         }
 
         #[test]
         fn post() {
-            let db = seeded_db();
+            let mut db = seeded_db();
             db.post("hello").ok().unwrap();
-            db.nuke().ok().unwrap();
+            nuke_db(db);
         }
 
         #[test]
         fn get() {
-            let db = seeded_db();
+            let mut db = seeded_db();
             let id = db.post::<String>("hello".to_string()).ok().unwrap();
             let actual = db.get::<String>(&id).ok().unwrap();
             assert_eq!(actual, "hello");
-            db.nuke().ok().unwrap();
+            nuke_db(db);
         }
 
         #[test]
         fn multiple_get() {
-            let db = seeded_db();
+            let mut db = seeded_db();
             let id1 = db.post::<String>("hello1".to_string()).ok().unwrap();
             let actual1 = db.get::<String>(&id1).ok().unwrap();
             assert_eq!(actual1, "hello1");
             let id2 = db.post::<String>("hello2".to_string()).ok().unwrap();
             let actual2 = db.get::<String>(&id2).ok().unwrap();
             assert_eq!(actual2, "hello2");
-            db.nuke().ok().unwrap();
+            nuke_db(db);
         }
 
         #[test]
         fn nuke() {
-            let db = seeded_db();
+            let mut db = seeded_db();
             let id = db.post::<String>("hello".to_string()).ok().unwrap();
             let actual = db.nuke();
             assert!(actual.is_ok());
             assert!(db.get::<String>(&id).is_err());
-            db.nuke().ok().unwrap();
+            nuke_db(db);
         }
 
         #[test]
         fn delete() {
-            let db = seeded_db();
+            let mut db = seeded_db();
             let id = db.post::<String>("hello".to_string()).ok().unwrap();
             assert!(db.get::<String>(&id).is_ok());
             db.delete::<String>(&id).ok().unwrap();
             assert!(db.get::<String>(&id).is_err());
-            db.nuke().ok().unwrap();
+            nuke_db(db);
         }
 
         #[test]
@@ -145,7 +165,7 @@ mod simple_db {
             let db = seeded_db();
             let result = db.delete::<String>(&"made_up".to_string());
             assert!(result.is_err());
-            db.nuke().ok().unwrap();
+            nuke_db(db);
         }
 
         #[test]
@@ -159,18 +179,18 @@ mod simple_db {
                 name: "Stefano".to_string(),
                 x: 34,
             };
-            let db = seeded_db();
+            let mut db = seeded_db();
             let id = db.post(&complex).ok().unwrap();
             let retrieved_complex = db.get::<Complex>(&id).ok().unwrap();
             assert_eq!(retrieved_complex, complex);
             db.delete::<Complex>(&id).ok().unwrap();
             assert!(db.get::<Complex>(&id).is_err());
-            db.nuke().ok().unwrap();
+            nuke_db(db);
         }
 
         #[test]
         fn find() {
-            let db = seeded_db();
+            let mut db = seeded_db();
             db.post::<String>("hello".to_string()).ok().unwrap();
             let actual = db
                 .find_one::<String>(|x: &String| x.starts_with("hell"))
@@ -178,19 +198,19 @@ mod simple_db {
                 .unwrap()
                 .unwrap();
             assert_eq!(actual, "hello");
-            db.nuke().ok().unwrap();
+            nuke_db(db);
         }
 
         #[test]
         fn not_found() {
-            let db = seeded_db();
+            let mut db = seeded_db();
             db.post::<String>("hello".to_string()).ok().unwrap();
             let actual = db
                 .find_one::<String>(|x: &String| x.starts_with("hellllooo"))
                 .ok()
                 .unwrap();
             assert!(actual.is_none());
-            db.nuke().ok().unwrap();
+            nuke_db(db);
         }
     }
 }
